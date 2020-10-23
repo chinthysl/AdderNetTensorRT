a2f5df26f3f136eff1234609b5b5c50d326767e8
chinthysl <gamanayaka.chinthaka@gmail.com>
2020-10-24 02:03:52 +0800

    Working Adder2dPlugin loading test using TRT network

--- plugin/test/test.py
+++ plugin/test/test.py
@@ -1,8 +1,10 @@
 import sys
 sys.path.append("../build")
+from adder2dpytrt import Adder2dPlugin
 
 import tensorrt as trt
 import numpy as np
+
 
 TRT_LOGGER = trt.Logger()
 
@@ -14,13 +16,14 @@ def get_adder2d_plugin():
     plugin = None
     plugin_name = "Adder2d_TRT"
     for plugin_creator in PLUGIN_CREATORS:
+        print(plugin_creator.name )
         if plugin_creator.name == plugin_name:
-            weight_field = trt.PluginField("weights", np.ones((128,), dtype=np.float32), trt.PluginFieldType.kFLOAT32)
-            nbWeights_field = trt.PluginField("nbWeights", 128, trt.PluginFieldType.kINT32)
-            filterSize_field = trt.PluginField("filterSize", 3, trt.PluginFieldType.kINT32)
-            nbFilters_field = trt.PluginField("nbFilters", 64, trt.PluginFieldType.kINT32)
-            stride_field = trt.PluginField("stride", 1, trt.PluginFieldType.kINT32)
-            padding_field = trt.PluginField("padding", 0, trt.PluginFieldType.kINT32)
+            weight_field = trt.PluginField("weights", np.ones((128,), dtype=np.float32), trt.PluginFieldType.FLOAT32)
+            nbWeights_field = trt.PluginField("nbWeights", np.array([128], dtype=np.int32), trt.PluginFieldType.INT32)
+            filterSize_field = trt.PluginField("filterSize", np.array([3], dtype=np.int32), trt.PluginFieldType.INT32)
+            nbFilters_field = trt.PluginField("nbfilter", np.array([64], dtype=np.int32), trt.PluginFieldType.INT32)
+            stride_field = trt.PluginField("stride", np.array([1], dtype=np.int32), trt.PluginFieldType.INT32)
+            padding_field = trt.PluginField("padding", np.array([0], dtype=np.int32), trt.PluginFieldType.INT32)
 
             field_collection = trt.PluginFieldCollection([weight_field, nbWeights_field, filterSize_field,
                                                           nbFilters_field, stride_field, padding_field])
@@ -32,31 +35,12 @@ def test_plugin():
     with trt.Builder(TRT_LOGGER) as builder, builder.create_network() as network:
         builder.max_workspace_size = 2**20
         input_tensor = network.add_input(name="input_data", dtype=trt.float32, shape=(3, 32, 32))
-        adder_layer = network.add_plugin_v2(inputs=input_tensor, plugin=get_adder2d_plugin())
+        adder_plugin = get_adder2d_plugin()
+        print(adder_plugin)
+        adder_layer = network.add_plugin_v2(inputs=[input_tensor], plugin=adder_plugin)
         adder_layer.get_output(0).name = "outputs"
         network.mark_output(adder_layer.get_output(0))
 
 
-def has_adder2d_plugin():
-    try:
-        from adder2dpytrt import Adder2dPlugin
-        print('Adder2dPlugin loaded from dynamic lib')
-        return True
-    except Exception as e:
-        print(e)
-        return False
-
-
-def get_adder2d_plugin_v2(size, mode, align_corners):
-    if(has_adder2d_plugin()):
-        PLUGIN_NAME = 'Adder2d_TRT'
-        registry = trt.get_plugin_registry()
-        creator = [c for c in registry.plugin_creator_list if c.name == PLUGIN_NAME and c.plugin_namespace == '_TRT'][0]
-        adder2d_plugin = Adder2dPlugin(size=size, mode=mode, align_corners=align_corners)
-        return creator.deserialize_plugin(PLUGIN_NAME, torch2trt_plugin.serializeToString())
-
-
 if __name__ == "__main__":
     test_plugin()
-
-
